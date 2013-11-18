7a6fb447bf0bd05a981a2745cf1ad00e85fc9d8e
jondell518 <jondell518@gmail.com>
2013-11-18 01:25:52 -0500

    map generated, random start and end, costs

--- main.js
+++ main.js
@@ -5,8 +5,17 @@ var c = document.getElementById("mycanvas");
 var ctx = c.getContext("2d");
 
 
+
+
 var squareDim = 50;
 var squareOffset = 2;
+
+var squares = [];
+var playerCost = 0;
+
+var rndX =0;
+var rndY = 0;
+
 
 
 
@@ -15,6 +24,8 @@ var square = function(x,y,type)
 	this.x = x;
 	this.y = y;
 	this.type = type;
+	this.isBuilt = false;
+	
 
 	this.draw = function(color)
 	{
@@ -28,7 +39,7 @@ var World = function()
 {
 	//Sets up the world, by generating a random map
 
-	//0 is flat, 1 is mountain, 2 is water, 3 is forest
+	//0 is flat, 2 is mountain, 3 is water, 1 is forest
 	this.types = [0,1,2,3];
 
 	//number of cells wide
@@ -38,14 +49,21 @@ var World = function()
 	this.height = 16;
 
 	//array of the world
-	this.squares = [];
-
-	this.flat =0;
-	this.mountain =0;
-
+	this.squares = squares;
 	this.generateMap();
-	console.log(this.flat + " " + this.mountain);
-	this.update();
+	console.log("MAP INTIALIZED");
+	this.drawMap();
+	console.log("MAP DRAWN");
+	this.generateStart();
+	this.generateGoal();
+	console.log("START AND END GENERATED");
+
+	c.addEventListener('click',function(evt){
+    	var indexX = Math.floor(evt.clientX/(squareDim+squareOffset));
+    	var indexY = Math.floor(evt.clientY/(squareDim+squareOffset));
+   	 	squares[indexX][indexY].draw("#838B8B");
+   	 	playerCost += squares[indexX][indexY].cost;
+   	});
 
 }
 
@@ -63,7 +81,7 @@ World.prototype.generateMap = function()
 	}
 
 
-	//generate 10 blocks of each type of tile
+	//generate 10 blocks of each type of tile, rest are flat
 	for(var i =0; i< 10; i++)
 	{
 		var waterX = Math.floor(Math.random()*32);
@@ -73,13 +91,57 @@ World.prototype.generateMap = function()
 		var forestX = Math.floor(Math.random()*32);
 		var forestY = Math.floor(Math.random()*16);
 
-		this.squares[waterX][waterY].type = 2;
-		this.addTiles(waterX,waterY,2);
-		this.squares[mountainX][mountainY].type = 1;
-		this.addTiles(mountainX,mountainY,1);
-		this.squares[forestX][forestY].type = 3;
-		this.addTiles(forestX,forestY, 3);
+		this.squares[waterX][waterY].type = 3;
+		this.addTiles(waterX,waterY,3);
+		this.squares[mountainX][mountainY].type = 2;
+		this.addTiles(mountainX,mountainY,2);
+		this.squares[forestX][forestY].type = 1;
+		this.addTiles(forestX,forestY, 1);
+	}
+
+	for(var i =0; i< this.width; i++)
+	{
 		
+		for(var j = 0; j <this.height; j++)
+		{
+			this.squares[i][j].cost = (this.squares[i][j].type*5)+5	
+		}
+	}
+}
+
+World.prototype.generateStart = function()
+{
+	var notFound = false;
+	while(!notFound)
+	{
+		rndX = Math.floor(Math.random()*this.width);
+		rndY = Math.floor(Math.random()*this.height);
+
+		if(rndX == 0 || rndY == 0 || rndX == this.width-1 || rndY == this.height-1)
+		{
+			notFound = true;
+			squares[rndX][rndY].draw("#838B8B");
+		}
+	}
+}
+
+World.prototype.generateGoal = function()
+{
+	var notFound = false;
+	while(!notFound)
+	{
+		var newRX = Math.floor(Math.random()*32);
+		var newRY = Math.floor(Math.random()*16);
+
+		if(newRX != rndX && newRY != rndY)
+		{
+			if(newRX == 0 || newRY == 0 || newRX == this.width-1 || newRY == this.height-1)
+			{
+				notFound = true;
+				squares[newRX][newRY].draw("#838B8B");
+			}
+		}
+
 		
 	}
 }
@@ -89,7 +151,7 @@ World.prototype.generateMap = function()
 
 
 
-World.prototype.update = function()
+World.prototype.drawMap = function()
 {
 
 	for(var i =0; i < this.squares.length; i++)
@@ -103,26 +165,26 @@ World.prototype.update = function()
 			}
 			else if(this.squares[i][j].type == 1)
 			{
-				this.squares[i][j].draw('#310c0c');
+				
+				this.squares[i][j].draw('#002013');
 			}
 			else if(this.squares[i][j].type == 2)
 			{
-				this.squares[i][j].draw('#2cabe2');
+				
+				this.squares[i][j].draw('#310c0c');
 			}
 			else if(this.squares[i][j].type == 3)
 			{
 				
-				this.squares[i][j].draw('#002013');
+				this.squares[i][j].draw('#2cabe2');
 			}
-
-			
 		}
 	}
 }
 
 World.prototype.addTiles = function(X,Y,type)
 {
-	if(X > 0 && X < 31 && Y > 1 && Y < 15)
+	if(X > 0 && X < 31 && Y > 0 && Y < 15)
 	{
 		this.squares[X-1][Y].type =type;
 		this.squares[X-1][Y-1].type = type;
